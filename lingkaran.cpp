a324bc33cc0a2414cc0caf45db53457418a221e3
muhammadyusufalfaqih <45525489+muhammadyusufalfaqih@users.noreply.github.com>
2018-12-30 14:57:20 -0800

    Add files via upload

---
+++ lingkaran.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int r;
+    float phi=3.14,luas;
+
+    cout<<"masukan jari jari lingkaran:";
+    cin>>r;
+
+    luas=phi*r*r;
+    cout<<"luas lingkaran adalah"<<luas;
+    std::cout<<" \n";
+
+    std::cout<<"nama : muhammad yusuf alfaqih \n";
+    std::cout<<"nim  : 311810239";
+    return 0;
+}
